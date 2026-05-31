4a2bea2e5ebd70872dadf90bd668204dded8f724
Teo <none>
2026-05-31 22:33:11 +0800

    Added Fixes

--- scripts/timer.js
+++ scripts/timer.js
@@ -3,8 +3,38 @@ let spinnerDown = document.getElementById('spinnerdown');
 let timerInput = document.getElementById('timerinput');
 let timer = document.getElementById('timer');
 
-timerInput.value = "00"
+timerInput.value = "00";
 
 spinnerUp.addEventListener("click", () => {
-    
+    let currentValue = Number(timerInput.value);
+    let newValue;
+
+    newValue = currentValue + 5;
+
+    if (newValue > 60) {
+        return "stop";
+    }
+
+    if (newValue < 10) {
+        newValue = "0" + newValue;
+    }
+
+    timerInput.value = newValue;
+})
+
+spinnerDown.addEventListener("click", () => {
+    let currentValue = Number(timerInput.value);
+    let newValue
+
+    newValue = currentValue - 5;
+
+    if (newValue < 0) {
+        return "stop";
+    }
+
+    if (newValue < 10) {
+        newValue = "0" + newValue;
+    }
+
+    timerInput.value = newValue;
 })
