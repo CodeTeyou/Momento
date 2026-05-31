b0c91b3cad70f4d834ff2112380b9ece211d345f
Teo <none>
2026-05-31 22:27:35 +0800

    Timer Adjusments

---
+++ scripts/timer.js
@@ -0,0 +1,10 @@
+let spinnerUp = document.getElementById('spinnerup');
+let spinnerDown = document.getElementById('spinnerdown');
+let timerInput = document.getElementById('timerinput');
+let timer = document.getElementById('timer');
+
+timerInput.value = "00"
+
+spinnerUp.addEventListener("click", () => {
+    
+})
