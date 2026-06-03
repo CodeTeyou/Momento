94fe737ab89f557e4fb9abb7c74a4689c119dd32
Teo <none>
2026-06-03 17:02:48 +0800

    Added Saving Workframe

---
+++ scripts/keynotes.js
@@ -0,0 +1,30 @@
+let noteOptions = document.getElementById("savednotes");
+let addOption = document.getElementById("add");
+let noteNameInput = document.getElementById("notename");
+let save = document.getElementById("save");
+let keyNote = document.getElementById("keynotes");
+
+const datarequest = indexedDB.open("KeyNotes");
+let database;
+
+datarequest.onupgradeneeded = (event) => {
+    database = event.target.result;
+
+    database.createObjectStore("notesList", {
+        keyPath: "id"
+    });
+}
+
+datarequest.onsuccess = (event) => {
+    database = event.target.result;
+}
+
+datarequest.onerror = (event) => {
+    console.warn(event);
+}
+
+save.addEventListener("click", (clicks) => {
+    const transaction = database.transaction(["notesList"], "readwrite");
+    const store = transaction.objectStore("notesList");
+    store.add()
+});
