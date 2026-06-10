854aeb4740b7a136c94410a81fb0e019395c6e70
Teo <limboc.teo@gmail.com>
2026-06-10 16:04:50 +0800

    added

---
+++ scripts/textEditor.js
@@ -0,0 +1,59 @@
+const addButton = document.getElementById("addSection");
+const documentWriter = document.getElementById("documentWriter");
+
+// Create Section
+function createSection(parentElement) {
+  if (!parentElement) {
+    console.error("Missing Value");
+    return;
+  }
+  let sectionCount = document.getElementsByClassName("documentSection");
+
+  const newSection = document.createElement("section");
+  const sectionHead = document.createElement("b");
+  const sectionContent = document.createElement("ul");
+
+  newSection.className = "documentSection";
+  newSection.id = `section-${sectionCount.length + 1}`;
+
+  sectionHead.textContent = "New Section";
+  sectionHead.contentEditable = true;
+
+  parentElement.appendChild(newSection);
+  newSection.appendChild(sectionHead);
+  newSection.appendChild(sectionContent);
+
+  const firstItem = document.createElement("li");
+  firstItem.textContent = "";
+  firstItem.contentEditable = true;
+
+  sectionContent.appendChild(firstItem);
+
+  sectionContent.addEventListener("keydown", (keyPressed) => {
+    const currentItem = event.target
+
+    if (keyPressed.key === "Enter") {
+      keyPressed.preventDefault();
+      const newListItem = document.createElement("li");
+      newListItem.contentEditable = true;
+      sectionContent.appendChild(newListItem);
+      newListItem.focus()
+    }
+
+    if (keyPressed.key === "Backspace") {
+      if (currentItem.textContent.trim() == "") {
+        keyPressed.preventDefault();
+        let previous = currentItem.previousElementSibling;
+
+        if (previous) {
+          currentItem.remove();
+          previous.focus()
+        }
+      }
+    }
+  });
+}
+
+addButton.addEventListener("click", () => {
+  createSection(documentWriter);
+});
