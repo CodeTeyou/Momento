e30e18cd59a6684c4428a2650e324c9a72a5ce58
Teo <limboc.teo@gmail.com>
2026-06-25 21:08:33 +0800

    Page Adding System

--- scripts/textEditor.js
+++ scripts/textEditor.js
@@ -6,12 +6,28 @@ const getElement = (elementId) => {
 const documentWriter = getElement("documentWriter");
 const editorSettings = getElement("editorSettings");
 
+// Constructor
+function makeElement(elementType, editable, placeHolder, classes) {
+  const newElement = document.createElement(elementType);
+  newElement.contentEditable = editable;
+  newElement.className = classes;
+  try {
+    newElement.innerText = placeHolder;
+  } catch (err) {
+    console.error(err);
+  }
+  return newElement;
+}
+
 // Page creator
 
 let pageList = [];
 
 class Page {
   element;
+  head = makeElement("h1", true, "New Page", "pageHeader");
+  content = makeElement("div", true, null, "pageContent");
+  foot = makeElement("footer", true, "Footer", "pageFooter");
 
   constructor(pageId, parentElement) {
     const newPage = document.createElement("section");
@@ -19,26 +35,24 @@ class Page {
     newPage.className = "documentPage";
     parentElement.appendChild(newPage);
 
+    this.head.className = "pageHeader";
+
+    this.content;
+
+    this.foot.className = "pageFooter";
+
+    newPage.appendChild(this.head);
+    newPage.appendChild(this.content);
+    newPage.appendChild(this.foot);
+
     this.element = newPage;
-    pageList.push(newPage);
+    pageList.push(this);
   }
 
   stands() {
     const tempCheck = this.element ? this.element : null;
     return tempCheck;
   }
-}
-
-// Constructor
-function makeElement(elementType, editable, placeHolder) {
-  const newElement = document.createElement(elementType);
-  newElement.contentEditable = editable;
-  try {
-    newElement.innerText = placeHolder;
-  } catch (err) {
-    console.error(err);
-  }
-  return newElement;
 }
 
 // Previous Page Render
@@ -49,19 +63,12 @@ const firstPage = new Page(
   documentWriter,
 );
 
-// !! Make a page sizing system
-
 // Function List
 const editorActions = {
-  addSection() {
-    console.log("Add Section: ");
-  },
-  addTitle() {
-    console.log("Add Title: ");
-
-  },
-  addHeader() {
-    console.log("Add Footer: ");
+  addSection() {},
+  addTitle() {},
+  addPage() {
+    const newPageMade = new Page(String(pageList.length + 1), documentWriter);
   },
   boldText() {
     document.execCommand("bold");
@@ -75,7 +82,6 @@ const editorActions = {
 };
 
 // Button Registration
-
 editorSettings.addEventListener("click", (clickEvent) => {
   const clickId = clickEvent.target.id;
   if (typeof editorActions[clickId] === "function") {
